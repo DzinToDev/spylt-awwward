b250b7a6c2b9da087538e24cd4a68061545ba69a
DzinToDev <maheshdgaikwad18@gmail.com>
2025-07-04 01:03:56 +0530

    flavorSection done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
 import { ScrollTrigger, gsap } from 'gsap/all'
 import MessageSection from './sections/MessageSection'
+import FlavourSection from './sections/FlavorSection'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <HeroSection />
      <MessageSection />
+     <FlavourSection />
     </main>
   )
 }
---
+++ src/components/FlavorSlider.jsx
@@ -0,0 +1,61 @@
+import { useGSAP } from "@gsap/react";
+import { flavorlists } from "../constants";
+import { gsap } from "gsap/all";
+import { useRef } from "react";
+
+const FlavorSlider = () => {
+  const sliderRef = useRef();
+
+  useGSAP(() => {
+    const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
+    // console.log(scrollAmount);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "2% top",
+        end: `+=${scrollAmount + 1100}px`,
+        scrub: true,
+        pin: true,
+        markers: true,
+      },
+    });
+    tl.to(".flavor-section", {
+      x: `-${scrollAmount + 1100}px`,
+      ease: "power1.inOut",
+    });
+  });
+  return (
+    <div ref={sliderRef} className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+          <div
+            key={flavor.name}
+            className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:h-[90vw] h-80 flex-none ${flavor.rotation}`}
+          >
+            <img
+              src={`/images/${flavor.color}-bg.svg`}
+              alt=""
+              className="absolute bottom-0"
+            />
+            <img
+              src={`/images/${flavor.color}-drink.webp`}
+              alt=""
+              className="drinks"
+            />
+
+            <img
+              src={`/images/${flavor.color}-elements.webp`}
+              alt=""
+              className="elements"
+            />
+
+            <h1>{flavor.name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FlavorSlider;
